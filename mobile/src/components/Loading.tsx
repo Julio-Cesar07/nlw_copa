7fcf349b3654537ba328acd26e2afa638f8fc5bb
Julio-Cesar07 <julio.oliveira@catijr.com.br>
2022-11-01 04:43:19 -0300

    init

---
+++ mobile/src/components/Loading.tsx
@@ -0,0 +1,7 @@
+import { Center, Spinner} from 'native-base';
+
+export function Loading(){
+    return (
+        <Spinner color="yellow.500" />
+    )
+}
---
+++ mobile/src/screens/SignIn.tsx
@@ -0,0 +1,7 @@
+import { Center, Text} from 'native-base';
+
+export function SignIn(){
+    return (
+        <Text color="white" fontSize={24}>SignIn</Text>
+    )
+}
